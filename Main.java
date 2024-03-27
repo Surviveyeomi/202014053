84aeec0bfc3cefb7cf80ef2c8ce473029c05476e
Surviveyeomi <yeomi189@gmail.com>
2024-03-27 10:34:55 +0900

    four

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ package 202014053;
 public class Main {
    public static void main(String[] args) {
      System.out.println("one 202014053"); 
+     Three.print(); 
+     Four.print();
     } 
   }
